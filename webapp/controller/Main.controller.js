08011f9be73a88003bff272935e863751928e082
Jason Patigayon <jason.patigayon@accenture.com>
2025-08-19 15:33:42 +0000

    Hello commit final

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,17 +9,17 @@ sap.ui.define([
     return Controller.extend("sapips.training.odata.controller.Main", {
         onInit: function () {
             // get the table by ID
-            var oTable = this.byId("productTable");
+            let oTable = this.byId("productTable");
 
             // get the item binding
-            var oBinding = oTable.getBinding("items");
+            let oBinding = oTable.getBinding("items");
 
             if (oBinding) {
                 // sort by product name
-                var oSorter = new Sorter("ProductName", false);
+                let oSorter = new Sorter("ProductName", false);
 
                 // exclude discontinued
-                var oFilter = new Filter("Discontinued", FilterOperator.EQ, false);
+                let oFilter = new Filter("Discontinued", FilterOperator.EQ, false);
 
                 // sorting and filtering
                 oBinding.sort(oSorter);
